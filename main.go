bebeaf31e272e44f773033e3e9c6b196e2c29a6b
Agustín Ezequiel Alvarez <agusalvarez99@hotmail.com>
2023-08-05 20:28:31 -0300

    hasta prueba de envio de mail DONE

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"context"
+	"database/sql"
+	"encoding/base64"
 	"encoding/json"
 	"fmt"
 	"log"
@@ -11,11 +13,11 @@ import (
 	"net/http"
 	"os"
 
-	//"strings"
-
+	_ "github.com/go-sql-driver/mysql"
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/google"
 	"google.golang.org/api/drive/v3"
+	"google.golang.org/api/gmail/v1"
 	"google.golang.org/api/option"
 )
 
@@ -81,7 +83,7 @@ func main() {
 	}
 
 	// si modifico el scope tengo que eliminar el token.json viejo
-	config, err := google.ConfigFromJSON(b, drive.DriveMetadataReadonlyScope)
+	config, err := google.ConfigFromJSON(b, drive.DriveMetadataScope, gmail.GmailSendScope)
 	if err != nil {
 		log.Fatalf("No se pudo analizar el archivo client secret: %v", err)
 	}
@@ -92,15 +94,21 @@ func main() {
 		log.Fatalf("No se pudo recuperar el cliente de drive: %v", err)
 	}
 	//el pagesize me limita la cantidad de archivos para mostrar
-	r, err := srv.Files.List().PageSize(5).
+	r, err := srv.Files.List().PageSize(2).
 		Fields("nextPageToken, files(id, name, fileExtension, owners)").Do()
 	if err != nil {
 		log.Fatalf("No se pudieron recuperar los archivos: %v", err)
 	}
-	fmt.Println("Archivos:")
+	fmt.Println("App by agusalvarez:")
 	if len(r.Files) == 0 {
 		fmt.Println("No se encontraron archivos")
 	} else {
+		fmt.Print("Indique 1 si solo quiere observar los datos o 2 si quiere ademas almacenarlos en la base:")
+		var choice string
+		_, err := fmt.Scanf("%s", &choice)
+		if err != nil {
+			log.Fatalf("No se pudo leer la entrada: %v", err)
+		}
 		for _, i := range r.Files {
 			//para determinar si es publico o privado
 			permissions, err := srv.Permissions.List(i.Id).Do()
@@ -126,7 +134,80 @@ func main() {
 			} else if strings.Contains(file.MimeType, "google") {
 				fileExtension = "Documento de Google"
 			}
-			fmt.Printf("ID: %s\nNombre: %s\nExtensión: %s\nDueño: %s\nVisibilidad: %s\n\n", i.Id, i.Name, fileExtension, i.Owners[0].EmailAddress, visibility)
+			//imprimir por pantalla los datos de los archivos obtenidos
+			fmt.Printf("\nID: %s\nNombre: %s\nExtensión: %s\nDueño: %s\nVisibilidad: %s\n\n", i.Id, i.Name, fileExtension, i.Owners[0].EmailAddress, visibility)
+			if choice == "2" {
+				insertFile(i.Id, i.Name, fileExtension, i.Owners[0].EmailAddress, visibility)
+			}
 		}
 	}
+	sendEmail()
+}
+
+func insertFile(id string, nombre string, extension string, dueño string, visbilidad string) {
+	dsn := "root:agustin@tcp(localhost:3306)/inventario"
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		log.Fatalf("No se pudo conectar a la base: %v", err)
+	}
+	defer db.Close()
+
+	_, err = db.Exec("CALL insert_file(?, ?, ?, ?, ?, ?)", id, nombre, extension, dueño, visbilidad, nil)
+	if err != nil {
+		log.Fatalf("No se pudo insertar los datos: %v", err)
+	}
+
+	fmt.Printf("Datos insertados correctamente!\n")
+
+}
+
+func sendEmail() {
+	ctx := context.Background()
+	b, err := os.ReadFile("credentials.json")
+	if err != nil {
+		log.Fatalf("No se pudo leer el archivo client secret: %v", err)
+	}
+
+	// configuracion de la autenticacion, en caso de modificar el scope debo borrar el token.json
+	config, err := google.ConfigFromJSON(b, gmail.GmailSendScope)
+	if err != nil {
+		log.Fatalf("No se pudo analizar el archivo client secret: %v", err)
+	}
+	client := getClient(config)
+
+	srv, err := gmail.NewService(ctx, option.WithHTTPClient(client))
+	if err != nil {
+		log.Fatalf("No se pudo recuperar el cliente de Gmail: %v", err)
+	}
+
+	//mensaje a enviar
+	to := "agus99test@gmail.com"
+	subject := "Cuestionario de seguridad de archivo X"
+	body := "enlace al google form"
+	message := createMessage(to, subject, body)
+
+	//envio del mensaje
+	user := "me"
+	_, err = srv.Users.Messages.Send(user, &message).Do()
+	if err != nil {
+		log.Fatalf("No se pudo enviar el mail: %v", err)
+	}
+	fmt.Println("Correo enviado correctamente!")
+}
+
+func createMessage(to, subject, body string) gmail.Message {
+	msg := gmail.Message{}
+	headers := make(map[string]string)
+	headers["To"] = to
+	headers["Subject"] = subject
+	headers["Content-Type"] = "text/html; charset\"utf-8\""
+
+	var msgString string
+	for k, v := range headers {
+		msgString += fmt.Sprintf("%s: %s\r\n", k, v)
+	}
+	msgString += "\r\n" + body
+
+	msg.Raw = base64.URLEncoding.EncodeToString([]byte(msgString))
+	return msg
 }
