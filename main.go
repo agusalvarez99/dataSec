c53dbb413dffdf8fb326ec3b2188914bed78f7f9
Agustín Ezequiel Alvarez <agusalvarez99@hotmail.com>
2023-08-06 21:57:05 -0300

    clasificacion archivos DONE

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"database/sql"
+
 	"encoding/base64"
 	"encoding/json"
 	"fmt"
@@ -19,6 +20,7 @@ import (
 	"google.golang.org/api/drive/v3"
 	"google.golang.org/api/gmail/v1"
 	"google.golang.org/api/option"
+	"google.golang.org/api/sheets/v4"
 )
 
 // obtiene el token, lo guarda y retorna el client http
@@ -44,7 +46,7 @@ func getTokenFromWeb(config *oauth2.Config) *oauth2.Token {
 	if _, err := fmt.Scan(&authCode); err != nil {
 		log.Fatalf("No se pudo leer el codigo de autorizacion %v", err)
 	}
-
+	fmt.Scanln()
 	tok, err := config.Exchange(context.TODO(), authCode)
 	if err != nil {
 		log.Fatalf("No se pudo recuperar el token desde la web %v", err)
@@ -83,7 +85,7 @@ func main() {
 	}
 
 	// si modifico el scope tengo que eliminar el token.json viejo
-	config, err := google.ConfigFromJSON(b, drive.DriveMetadataScope, gmail.GmailSendScope)
+	config, err := google.ConfigFromJSON(b, drive.DriveMetadataScope, gmail.GmailSendScope, sheets.SpreadsheetsReadonlyScope)
 	if err != nil {
 		log.Fatalf("No se pudo analizar el archivo client secret: %v", err)
 	}
@@ -100,15 +102,10 @@ func main() {
 		log.Fatalf("No se pudieron recuperar los archivos: %v", err)
 	}
 	fmt.Println("App by agusalvarez:")
+	fmt.Println("A continuacion se mostraran los archivos recorridos de a uno por vez!")
 	if len(r.Files) == 0 {
 		fmt.Println("No se encontraron archivos")
 	} else {
-		fmt.Print("Indique 1 si solo quiere observar los datos o 2 si quiere ademas almacenarlos en la base:")
-		var choice string
-		_, err := fmt.Scanf("%s", &choice)
-		if err != nil {
-			log.Fatalf("No se pudo leer la entrada: %v", err)
-		}
 		for _, i := range r.Files {
 			//para determinar si es publico o privado
 			permissions, err := srv.Permissions.List(i.Id).Do()
@@ -136,12 +133,40 @@ func main() {
 			}
 			//imprimir por pantalla los datos de los archivos obtenidos
 			fmt.Printf("\nID: %s\nNombre: %s\nExtensión: %s\nDueño: %s\nVisibilidad: %s\n\n", i.Id, i.Name, fileExtension, i.Owners[0].EmailAddress, visibility)
-			if choice == "2" {
+			//preguntar si desea guardarlo
+			fmt.Print("Indique Y si desea guardar los metadatos del archivo en la base de datos de lo contrario ingrese N: ")
+			var choice string
+			_, err = fmt.Scanln(&choice)
+			if err != nil {
+				log.Fatalf("No se pudo leer la entrada: %v", err)
+			}
+			if choice == "y" || choice == "Y" {
 				insertFile(i.Id, i.Name, fileExtension, i.Owners[0].EmailAddress, visibility)
 			}
+			//preguntar si desea enviar por correo las preguntas
+			fmt.Println("Desea enviar por correo las preguntas de seguridad? y/N")
+			var option string
+			// fmt.Scanln()
+			_, err = fmt.Scanln(&option)
+			if err != nil {
+				log.Fatalf("No se pudo leer la entrada: %v", err)
+			}
+			if option == "y" || option == "Y" {
+				sendEmail(i.Id, i.Name, i.Owners[0].EmailAddress)
+			}
+		}
+
+		fmt.Println("Si ya ha enviado todos los correos y las preguntas han sido contestadas puede procesar las mismas!")
+		fmt.Println("Ingrese Y si desea procesar las repuestas o N si desea terminar la ejecucion del programa")
+		var choice string
+		fmt.Scanln(&choice)
+		if choice == "Y" || choice == "y" {
+			scanResults()
+
+		} else if choice == "N" || choice == "n" {
+			os.Exit(0)
 		}
 	}
-	sendEmail()
 }
 
 func insertFile(id string, nombre string, extension string, dueño string, visbilidad string) {
@@ -161,7 +186,7 @@ func insertFile(id string, nombre string, extension string, dueño string, visbi
 
 }
 
-func sendEmail() {
+func sendEmail(idArchivo, nombreArchivo, dueño string) {
 	ctx := context.Background()
 	b, err := os.ReadFile("credentials.json")
 	if err != nil {
@@ -181,9 +206,9 @@ func sendEmail() {
 	}
 
 	//mensaje a enviar
-	to := "agus99test@gmail.com"
-	subject := "Cuestionario de seguridad de archivo X"
-	body := "enlace al google form"
+	to := dueño
+	subject := "Cuestionario de seguridad de archivo " + nombreArchivo + " (" + idArchivo + ")"
+	body := "https://forms.gle/bPSC2wxnyCQATwDC9"
 	message := createMessage(to, subject, body)
 
 	//envio del mensaje
@@ -210,4 +235,94 @@ func createMessage(to, subject, body string) gmail.Message {
 
 	msg.Raw = base64.URLEncoding.EncodeToString([]byte(msgString))
 	return msg
+}
+
+func scanResults() {
+	ctx := context.Background()
+	b, err := os.ReadFile("credentials.json")
+	if err != nil {
+		log.Fatalf("No se pudo leer el client secret: %v", err)
+	}
+
+	config, err := google.ConfigFromJSON(b, sheets.SpreadsheetsScope)
+	if err != nil {
+		log.Fatalf("No se pudo analizar el client secret: %v", err)
+	}
+
+	client := getClient(config)
+
+	srv, err := sheets.NewService(ctx, option.WithHTTPClient(client))
+	if err != nil {
+		log.Fatalf("No se pudo recuperar el cliente de Sheet: %v", err)
+	}
+
+	// ID de la hoja de cálculo y rango
+	spreadsheetId := "1XH9MnirbQcUglQo6TwOtEzlDGTI5Hl3RvaUVypgFhvU"
+	// rango para recorrer
+	readRange := "Respuestas!C2:H"
+
+	resp, err := srv.Spreadsheets.Values.Get(spreadsheetId, readRange).Do()
+	if err != nil {
+		log.Fatalf("No se pudo obtener los datos de la sheet: %v", err)
+	}
+
+	if len(resp.Values) == 0 {
+		fmt.Println("No se encontraron datos!")
+	} else {
+		//ACA TENGO QUE RECORRER LAS FILAS
+		for _, row := range resp.Values {
+			cellBlank := false
+			// el id esta en la columna C
+			id := row[0].(string)
+			// recorrer los valores de las columnas D a H y calcular puntaje
+			var puntaje int
+			for i := 1; i <= 5; i++ {
+				respuesta := strings.TrimSpace(strings.ToUpper(row[i].(string)))
+				if respuesta == "NO" {
+					puntaje += 0
+				} else if respuesta == "NS/NC" {
+					puntaje += 1
+				} else if respuesta == "SI" {
+					puntaje += 2
+				} else if respuesta == "" {
+					cellBlank = true
+					break
+				}
+			}
+			if cellBlank {
+				break
+			}
+
+			// asociar nivel de criticidad segun el puntaje
+			var nivelCriticidad string
+			switch {
+			case puntaje >= 0 && puntaje <= 2:
+				nivelCriticidad = "BAJO"
+			case puntaje >= 3 && puntaje <= 5:
+				nivelCriticidad = "MEDIO"
+			case puntaje >= 6 && puntaje <= 8:
+				nivelCriticidad = "ALTO"
+			case puntaje >= 9 && puntaje <= 10:
+				nivelCriticidad = "CRITICO"
+			default:
+				nivelCriticidad = "Desconocido"
+			}
+
+			fmt.Printf("ID: %s, Puntaje: %d, Nivel de Criticidad: %s\n", id, puntaje, nivelCriticidad)
+
+			//  conexión con la base de datos MySQL
+			db, err := sql.Open("mysql", "root:agustin@tcp(localhost:3306)/inventario")
+			if err != nil {
+				log.Fatalf("No se pudo conectar a la base: %v", err)
+			}
+			defer db.Close()
+
+			_, err = db.Exec("CALL add_level(?, ?)", id, nivelCriticidad)
+			if err != nil {
+				log.Fatalf("No se pudo actualizar el registro: %v", err)
+			}
+		}
+
+	}
+
 }
